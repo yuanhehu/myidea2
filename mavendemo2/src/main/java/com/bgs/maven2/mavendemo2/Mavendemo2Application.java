cc021a5928b0b69ee52b9cfffff3e80c22e04b09
1111@qq.com <1554786320@qq.com>
2019-02-24 15:26:55 +0800

    Initial commit

---
+++ mavendemo2/src/main/java/com/bgs/maven2/mavendemo2/Mavendemo2Application.java
@@ -0,0 +1,13 @@
+package com.bgs.maven2.mavendemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavendemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mavendemo2Application.class, args);
+    }
+
+}
---
+++ src/com/bgs/java/HelloWord.java
@@ -0,0 +1,9 @@
+package com.bgs.java;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("abc");
+        System.out.println("helloword");
+        System.out.println("helloword");
+    }
+}
